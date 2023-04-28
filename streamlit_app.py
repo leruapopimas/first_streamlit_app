afba816cb2e906f372bea512a881ed73c6842a71
leruapopimas <132055439+leruapopimas@users.noreply.github.com>
2023-04-28 11:20:31 +0200

    Update streamlit_app.py
    added breakfacst menu header, 3 items and changed title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
-streamlit.title('Bla bla')
+streamlit.title('My Parents New Healty Diner')
 
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
