4731623dd52a1133c9fc9b4fcba9236eb337963d
leruapopimas <132055439+leruapopimas@users.noreply.github.com>
2023-05-04 14:11:24 +0200

    add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
+streamlit.header("Fruityvice Fruit Advice!")
 
 streamlit.dataframe(fruits_to_show)
 
